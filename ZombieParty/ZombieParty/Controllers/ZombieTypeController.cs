69de18e18398a0e2c5bc60320bf90af4c1fad8f3
Lisa-Sommavethey Ly <1636083@cegepmontpetit.ca>
2026-09-05 20:46:57 -0400

    FCT Creation Views ZombieType Details
    Ajout de la view Detail affichant le ZombieType et la liste des zombies associés

--- ZombieParty/ZombieParty/Controllers/ZombieTypeController.cs
+++ ZombieParty/ZombieParty/Controllers/ZombieTypeController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ZombieParty.Models;
-
+using ZombieParty.ViewModels;
 namespace ZombieParty.Controllers
 {
 
@@ -40,9 +40,17 @@ namespace ZombieParty.Controllers
             return this.View(zombieType);
         }
         [HttpGet]
-        public IActionResult Details()
+        public IActionResult Details(int id)
         {
-            return View();
+            ZombieTypeVM zombieTypeVM = new()
+            {
+                ZombieType = new(),
+                ZombiesList = _baseDonnees.Zombies.Where(z => z.ZombieTypeId == id).ToList()
+            };
+
+            zombieTypeVM.ZombieType = _baseDonnees.ZombieTypes.FirstOrDefault(zt => zt.Id == id);
+            return View(zombieTypeVM);
+
         }
 
     }
